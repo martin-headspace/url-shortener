4ff4a042f1c78cae10f68271525fa1d1a4216405
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-09-29 21:31:47 -0500

    Desired UI Achieved
    Finished the main desired UI for the challenge. At this point, it's neccesary to solve the 'Hourglass' component being off-centered. An issue will be cast to Github

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,18 +1,88 @@
 import React from 'react'
-import { List, ListItem, Divider } from "react95"
+import { Window, WindowContent, WindowHeader, Button, Anchor, TextArea, Hourglass } from "react95"
+
+// Style JSON sheet
+const style = {
+    window: {
+        position:'fixed',
+        top:'50%',
+        left:'50%',
+        transform: 'translate(-50%,-50%)',
+        width: '400px'
+    },
+    windowHeader : {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    buttonmenu: {
+        marginRight: '-6px',
+        marginTop: '1px' 
+    },
+    button: {
+        marginTop: '3px',
+        left: '130px'
+    },
+    span: {
+        fontWeight: 'bold', 
+        transform: 'translateY(-1px)'
+    },
+    h1: {
+        fontFamily: 'times new roman',
+        textAlign: 'center',
+        fontSize: '3rem',
+        marginTop: '0.5rem',
+    },
+    p: {
+        fontFamily: 'times new roman',
+        textAlign: 'center',
+        marginBottom: '5px'
+    },
+    textarea: {
+        marginTop: '3px'
+    }
+}
 
 class App extends React.Component {
     state = {
-
+        submitState : false,
+        link : ''
     }
+
+    onSubmit = () => {
+        this.setState({submitState: true})
+    }
+
+    onWindowClose = () => {
+        window.open("about:blank", "_self");
+        window.close();
+    }
+    
     render() {
         return (
-            <List>
-                <ListItem>🎤 Sing</ListItem>
-                <ListItem>💃🏻 Dance</ListItem>
-                <Divider />
-                <ListItem disabled>😴 Sleep</ListItem>
-            </List>
+            <Window style={style.window }>
+                <WindowHeader style={style.windowHeader}>
+                    <span>URL Shortener</span>
+                    <Button onClick={this.onWindowClose} style={style.buttonmenu} size={'sm'} square>
+                        <span style={style.span}>x</span>
+                    </Button>
+                </WindowHeader>
+                <WindowContent>
+                    <h1 style={style.h1}>
+                         URL Shortener 
+                    </h1>
+                    <p style={style.p}> Created by <Anchor href="https://www.github.com/a01334390" target="_blank"> A01334390</Anchor></p>
+                    <TextArea disabled={this.state.submitState} onChange={(event) => this.setState({link: event.target.value})} style={style.textarea} shadow={true} placeholder="Place your link here..."></TextArea>
+                    <div style={style.button}>
+                    {!this.state.submitState ? 
+                        <Button onClick={this.onSubmit} style={style.button}>
+                             Convert 
+                        </Button> :
+                        <Hourglass/>
+                    }
+                    </div>
+                </WindowContent>
+            </Window>
         )
     }
 }
